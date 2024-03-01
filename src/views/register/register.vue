9d96e41351836a3d4e66eeae83593f141786fbe7
Miraitowa_zcx <2038322151@qq.com>
2024-03-01 20:10:14 +0800

    2024-03-01 20:09 add webp

--- src/views/register/register.vue
+++ src/views/register/register.vue
@@ -1,38 +1,44 @@
 <template>
-  <div style="height: 100vh; overflow: hidden; position: relative;">
+  <div style="height: 100vh; overflow: hidden; position: relative;" class="background">
     <el-card class="cover" v-if="registerAdmin.username">
       <slide-verify :l="42"
-            :r="10"
-            :w="310"
-            :h="155"
-            slider-text="向右滑动"
-            @success="onSuccess"
-            @fail="onFail"
-            @refresh="onRefresh">
+                    :r="10"
+                    :w="310"
+                    :h="155"
+                    slider-text="向右滑动"
+                    @success="onSuccess"
+                    @fail="onFail"
+                    @refresh="onRefresh">
       </slide-verify>
-      <div>{{msg}}</div>
+      <div>{{ msg }}</div>
     </el-card>
-    <div style="width: 500px; height: 450px; background-color: white; border-radius: 10px;
+    <el-row :style="{opacity: 0.8}">
+      <div style="width: 500px; height: 450px; background-color: white; border-radius: 10px;
             margin: 250px auto; padding: 50px;">
-      <div style="margin: 30px; text-align: center; font-size: 30px; font-weight: bold; color: dodgerblue;">注 册</div>
-      <el-form :model="admin" :rules="rules" ref="registerForm" label-width="80px">
-        <el-form-item prop="username" label="账号">
-          <el-input placeholder="请输入账号" prefix-icon="el-icon-user" size="medium" v-model="admin.username"></el-input>
-        </el-form-item>
-        <el-form-item prop="password" label="密码">
-          <el-input placeholder="请输入密码" show-password prefix-icon="el-icon-lock" size="medium" v-model="admin.password" type="password"></el-input>
-        </el-form-item>
-        <el-form-item prop="repeatPassword" label="确认密码">
-          <el-input placeholder="请输入确认密码" show-password prefix-icon="el-icon-lock" size="medium" v-model="admin.repeatPassword" type="password"></el-input>
-        </el-form-item>
-        <el-form-item style="margin-top: 30px">
-          <el-button style="width: 100%; " size="medium" type="primary" @click="register">注册</el-button>
-        </el-form-item>
-      </el-form>
-      <div style="text-align: center">
-        <el-link :underline="false" href="/login" target="_self">已有账号，去登录</el-link>
+        <div style="margin: 30px; text-align: center; font-size: 30px; font-weight: bold; color: dodgerblue;">注 册
+        </div>
+        <el-form :model="admin" :rules="rules" ref="registerForm" label-width="80px">
+          <el-form-item prop="username" label="账号">
+            <el-input placeholder="请输入账号" prefix-icon="el-icon-user" size="medium"
+                      v-model="admin.username"></el-input>
+          </el-form-item>
+          <el-form-item prop="password" label="密码">
+            <el-input placeholder="请输入密码" show-password prefix-icon="el-icon-lock" size="medium"
+                      v-model="admin.password" type="password"></el-input>
+          </el-form-item>
+          <el-form-item prop="repeatPassword" label="确认密码">
+            <el-input placeholder="请输入确认密码" show-password prefix-icon="el-icon-lock" size="medium"
+                      v-model="admin.repeatPassword" type="password"></el-input>
+          </el-form-item>
+          <el-form-item style="margin-top: 30px">
+            <el-button style="width: 100%; " size="medium" type="primary" @click="register">注册</el-button>
+          </el-form-item>
+        </el-form>
+        <div style="text-align: center">
+          <el-link :underline="false" href="/login" target="_self">已有账号，去登录</el-link>
+        </div>
       </div>
-    </div>
+    </el-row>
   </div>
 </template>
 
@@ -69,14 +75,14 @@ export default {
       admin: {},
       rules: {
         username: [
-          { required: true, message: '请输入账号', trigger: 'blur' },
-          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          {required: true, message: '请输入账号', trigger: 'blur'},
+          {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
         ],
         password: [
-          { validator: checkPass, required: true, trigger: 'blur' }
+          {validator: checkPass, required: true, trigger: 'blur'}
         ],
         repeatPassword: [
-            { validator: repeatPass, required: true, trigger: 'blur' }
+          {validator: repeatPass, required: true, trigger: 'blur'}
         ]
       }
     }
