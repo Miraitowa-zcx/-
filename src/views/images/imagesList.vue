63509260b246ab878fadcb4edab0be007e8d1eb3
Miraitowa_zcx <2038322151@qq.com>
2024-03-02 17:33:49 +0800

    2024-03-02 17:33 Update Grid

--- src/views/images/imagesList.vue
+++ src/views/images/imagesList.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="grid-container">
     <div v-for="item in gridItems" :key="item.id" class="grid-item">
-      {{ item.content }}
+      <el-tooltip placement="top">
+  <div slot="content">
+    <el-row>
+      <el-col :span="30" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 1 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <div style="padding: 0; text-align:center;">
+            <img :src="item.content" alt="图片"  width="130" height="130"/>
+            <el-descriptions :column="3" :size="size" border>
+              <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
+              <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
+              <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
+              <el-descriptions-item label="备注">
+                <el-tag size="small">学校</el-tag>
+              </el-descriptions-item>
+              <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+            </el-descriptions>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+  <img :src="item.content" alt="图片"  width="130" height="130"/>
+</el-tooltip>
     </div>
   </div>
 </template>
@@ -30,33 +52,33 @@ export default {
   data() {
     return {
       gridItems: [
-        {id: 1, content: 'Item 1'},
-        {id: 2, content: 'Item 2'},
-        {id: 3, content: 'Item 3'},
-        {id: 4, content: 'Item 4'},
-        {id: 5, content: 'Item 5'},
-        {id: 6, content: 'Item 6'},
-        {id: 7, content: 'Item 7'},
-        {id: 8, content: 'Item 8'},
-        {id: 9, content: 'Item 9'},
-        {id: 1, content: 'Item 1'},
-        {id: 2, content: 'Item 2'},
-        {id: 3, content: 'Item 3'},
-        {id: 4, content: 'Item 4'},
-        {id: 5, content: 'Item 5'},
-        {id: 6, content: 'Item 6'},
-        {id: 7, content: 'Item 7'},
-        {id: 8, content: 'Item 8'},
-        {id: 9, content: 'Item 9'},
-        {id: 1, content: 'Item 1'},
-        {id: 2, content: 'Item 2'},
-        {id: 3, content: 'Item 3'},
-        {id: 4, content: 'Item 4'},
-        {id: 5, content: 'Item 5'},
-        {id: 6, content: 'Item 6'},
-        {id: 7, content: 'Item 7'},
-        {id: 8, content: 'Item 8'},
-        {id: 9, content: 'Item 9'},
+        {id: 1, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 2, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 3, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 4, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 5, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 6, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 7, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 8, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 9, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 1, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 2, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 3, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 4, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 5, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 6, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 7, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 8, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 9, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 1, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 2, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 3, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 4, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 5, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 6, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 7, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 8, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
+        {id: 9, content: 'https://cdn.aqdstatic.com:966/poseidon/upload/vod/2020-09/166217058260836333.jpg'},
         // 更多项目...
       ]
     }
